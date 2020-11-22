defd28d363c1fc5c859424f90a56cc02464b04f4
JAUME PUIG <jaume.puig@skyscanner.net>
2020-11-22 17:50:29 +0100

    Updates

---
+++ precache-manifest.b152aaabe66aa7e0345c91a3e7bddd92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e404252b64584e02e69f75c20c766c3a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ac51b3fea94cae0aed9",
+    "url": "./static/css/main.074b9efb.chunk.css"
+  },
+  {
+    "revision": "80b6b259041a7d57dc38",
+    "url": "./static/js/2.635d6dea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.635d6dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac51b3fea94cae0aed9",
+    "url": "./static/js/main.ca9ea716.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.ca9ea716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca6a6ead5eb8212888d6",
+    "url": "./static/js/runtime-main.95876c1b.js"
+  }
+]);
